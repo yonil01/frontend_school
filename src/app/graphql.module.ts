1ea0b33bfb7d614c4feb88cf808cda7fc134c0c9
luis <luis.lucero@e-capture.co>
2021-06-10 15:55:11 -0500

    add apollo cliente

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,17 +1,42 @@
 import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import { ApolloClientOptions, ApolloLink, InMemoryCache } from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { setContext } from '@apollo/client/link/context';
 
 const uri = 'http://localhost:50030/query'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+
+  const auth = setContext((operation, context) => {
+    const token = sessionStorage.getItem('token');
+
+    if (token === null) {
+      return {};
+    } else {
+      return {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+    }
+  });
+
   return {
-    link: httpLink.create({uri}),
+    link: ApolloLink.from([basic, auth, httpLink.create({ uri })]),
     cache: new InMemoryCache(),
   };
 }
 
 @NgModule({
+  exports: [
+    HttpClientModule,
+  ],
   providers: [
     {
       provide: APOLLO_OPTIONS,
