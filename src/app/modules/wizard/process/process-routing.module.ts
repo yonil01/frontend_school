b254519c29e0572d0c051cf6ecfc511be1779f38
Luis Lucero <luis.lucero@e-capture.co>
2022-03-11 00:32:45 -0500

    Update layout module process

--- src/app/modules/wizard/process/process-routing.module.ts
+++ src/app/modules/wizard/process/process-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProcessListComponent} from "@app/modules/wizard/process/process/Pages/process-list/process-list.component";
 import {ProcessCreateComponent} from "@app/modules/wizard/process/process/Pages/process-create/process-create.component";
+import {ProcessShowComponent} from "@app/modules/wizard/process/process/Pages/process-show/process-show.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'create',
     component: ProcessCreateComponent,
+  },
+  {
+    path: 'show',
+    component: ProcessShowComponent,
   },
 ];
 
--- src/app/modules/wizard/process/process.module.ts
+++ src/app/modules/wizard/process/process.module.ts
@@ -6,13 +6,15 @@ import { ProcessComponent } from './process/process.component';
 import { ProcessListComponent } from './process/Pages/process-list/process-list.component';
 import {UiModule} from "@app/ui/ui.module";
 import { ProcessCreateComponent } from './process/Pages/process-create/process-create.component';
+import { ProcessShowComponent } from './process/Pages/process-show/process-show.component';
 
 
 @NgModule({
   declarations: [
     ProcessComponent,
     ProcessListComponent,
-    ProcessCreateComponent
+    ProcessCreateComponent,
+    ProcessShowComponent
   ],
   imports: [
     CommonModule,
