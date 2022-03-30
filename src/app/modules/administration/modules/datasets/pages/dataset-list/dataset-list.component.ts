cf04b1535d04b3bc2eb5cef92b6dc3c9579a0b6f
Santiago Perez <santiago.perez@e-capture.co>
2022-03-30 07:42:07 -0500

    Actualizacion funcionalidad lista

--- src/app/modules/administration/modules/datasets/pages/dataset-list/dataset-list.component.ts
+++ src/app/modules/administration/modules/datasets/pages/dataset-list/dataset-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Dataset} from "@app/core/models";
+import {cols, fieldTypes} from "@app/modules/administration/modules/datasets/utils/consts";
+import {DatasetsService} from "@app/modules/administration/modules/datasets/services/datasets.service";
 
 @Component({
   selector: 'app-dataset-list',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dataset-list.component.scss']
 })
 export class DatasetListComponent implements OnInit {
+  public datasets: Dataset[] = [];
+  public dataset?: Dataset;
+  public cols = cols;
+  public fieldTypes = fieldTypes;
 
-  constructor() { }
+  constructor(private datasetsService: DatasetsService,
+  ) {
+  }
 
   ngOnInit(): void {
+    this.getDatasets();
   }
+
+  private getDatasets(): void {
+    this.datasetsService.getDatasets().subscribe((res) => {
+      if (res.error) {
+        console.log("Hubo un error");
+      }
+      this.datasets = res.data ? JSON.parse(JSON.stringify(res.data)) : [];
+      console.log(this.datasets);
+    });
+  }
+
 
 }
