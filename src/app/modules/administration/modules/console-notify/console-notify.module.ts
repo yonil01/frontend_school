ed2fc4176919a71325c52ab3c4d29454f3da0628
Luis Lucero <luis.lucero@e-capture.co>
2022-05-22 18:16:28 -0500

    Update redirect

--- src/app/modules/administration/modules/console-notify/console-notify.module.ts
+++ src/app/modules/administration/modules/console-notify/console-notify.module.ts
@@ -5,6 +5,7 @@ import { ConsoleNotifyRoutingModule } from './console-notify-routing.module';
 import {ConsoleNotifyComponent} from "@app/modules/administration/modules/console-notify/console-notify.component";
 import {UiModule} from "@app/ui/ui.module";
 import {ToastModule} from "ecapture-ng-ui";
+import {TranslateModule} from "@ngx-translate/core";
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {ToastModule} from "ecapture-ng-ui";
         CommonModule,
         ConsoleNotifyRoutingModule,
         UiModule,
-        ToastModule
+        ToastModule,
+        TranslateModule
     ]
 })
 export class ConsoleNotifyModule { }
