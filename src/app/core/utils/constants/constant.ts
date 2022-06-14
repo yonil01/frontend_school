870a454e1b7f79671d801d32d25d85906730c102
Luis Lucero <luis.lucero@e-capture.co>
2022-06-14 16:46:22 -0500

    Update roles

--- src/app/core/utils/constants/constant.ts
+++ src/app/core/utils/constants/constant.ts
@@ -18,40 +18,89 @@ export const dataStepProcess = [
 
 export const ConfigElement = [
   {
+    id: 'a10016f7-3421-49e4-8d91-b613a00decf6',
     name: 'Entidades',
     icon: '../../../assets/img/entities-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a entidades.',
     route: 'entities',
   },
   {
+    id: '8e142376-b331-4255-a87a-60cb80141464',
     name: 'Documentos',
     icon: '../../../assets/img/documents-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a documentos.',
     route: 'documents',
   },
   {
+    id: '889dc0f4-99cb-4f7c-9e51-f37f8134a010',
     name: 'Formularios',
     icon: '../../../assets/img/forms-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a formularios.',
     route: 'dynamic-forms',
   },
   {
+    id: 'b7fe79fa-aad4-4c8c-89ab-8fdc04e42b02',
     name: 'BPMN',
     icon: '../../../assets/img/bpmns-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a procesos.',
     route: 'bpmn',
   },
   {
+    id: 'a5f4dff3-6d1a-4ac2-bc8f-398cb2367ec8',
     name: 'Roles',
     icon: '../../../assets/img/roles-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a roles.',
     route: 'roles',
   },
   {
+    id: '20426141-b9e6-4e83-801e-070543af71c7',
     name: 'Almacenamiento físico',
     icon: '../../../assets/img/storages-icon.svg',
     description: 'Presione aquí para configurar lo correspondiente a almacenamiento físico.',
     route: 'roles',
+  }
+]
+
+export const AdminElement = [
+  {
+    id: '84c729c0-66a1-4213-b2f8-5c0b58a7ece2',
+    name: 'Usuarios',
+    route: 'users',
+  },
+  {
+    id: 'd65d7929-7fa5-492a-974f-82ceb729290d',
+    name: 'Contraseñas no permitidas',
+    route: 'password-not-allowed',
+  },
+  {
+    id: '21f8ceca-cfb7-442f-8142-fa700b115ce0',
+    name: 'Mensajes',
+    route: 'message',
+  },
+  {
+    id: '1e7a7280-57d8-4e4c-b852-04ab9e5bdb22',
+    name: 'Conjunto de datos',
+    route: 'datasets',
+  },
+  {
+    id: '4cdef85f-b613-4b46-83c0-3ae05135adb9',
+    name: 'Calendarios',
+    route: 'calendar',
+  },
+  {
+    id: '53a63ec4-9b22-4805-b8d9-2b0b1f38fc04',
+    name: 'Notificaciones',
+    route: 'notification',
+  },
+  {
+    id: '82ea97f3-82e4-4681-b993-185a7f1f277f',
+    name: 'Temporizadores',
+    route: 'timer',
+  },
+  {
+    id: 'e75161d2-033e-4e54-9991-c05b2366636d',
+    name: 'Consola Diagnóstivo Worker',
+    route: 'console_notification',
   }
 ]
 
--- src/app/modules/wizard/wizard.component.ts
+++ src/app/modules/wizard/wizard.component.ts
@@ -7,8 +7,9 @@ import {Client, Project} from "@app/core/models/wizard/wizard";
 import {DropdownComponent, ToastService} from "ecapture-ng-ui";
 import {LocalStorageService} from "@app/core/services/local-storage/local-storage.service";
 import {Subscription} from "rxjs/internal/Subscription";
-import {ConfigElement} from "@app/core/utils/constants/constant";
+import {ConfigElement, AdminElement} from "@app/core/utils/constants/constant";
 import {HttpErrorResponse} from "@angular/common/http";
+import {RoleService} from "@app/modules/wizard/services/roles/role.service";
 
 @Component({
   selector: 'app-wizard',
@@ -24,16 +25,21 @@ export class WizardComponent implements OnInit, OnDestroy {
   public projectName: string = '';
   public clientName: string = '';
   public configList: any[] = [];
+  public adminList: any[] = [];
   //public isDisableGenerated: boolean = true;
   public isGenerate: boolean = false;
   public clientID: string = '';
   public projectID: string = '';
   public isBlockPage: boolean = false;
 
+  private modulesUserLogin: any[] = [];
+
   constructor(
     private _fb: FormBuilder,
     private customerService: CustomerService,
-    private _messageService: ToastService
+    private _messageService: ToastService,
+    private _sessionsService: LocalStorageService,
+    private _roleService: RoleService,
   ) {
     this.isBlockPage = true;
     this._subscription.add(
@@ -61,7 +67,41 @@ export class WizardComponent implements OnInit, OnDestroy {
         }
       })
     );
-    this.configList = ConfigElement;
+
+    this.modulesUserLogin = _sessionsService.getModules();
+
+    console.log(this.modulesUserLogin);
+
+    for(let module of this.modulesUserLogin){
+      if(module.id === "8e5df79b-7b22-4b2d-a3a2-0986224724e2"){ //Configuración
+        module.components[0].elements.forEach((element:any) => {
+          const confItem = ConfigElement.find((item) => item.id === element.id);
+          if(confItem){
+            this.configList.push(confItem);
+          }
+        });
+      }
+      if(module.id === "667c1c37-0d72-4ecc-945e-93ab97b4b0cc"){ //Administration
+        for(let component of module.components){
+          component.elements.forEach((element:any) => {
+            const adminItem = AdminElement.find((item) => item.id === element.id);
+            if(adminItem){
+              this.adminList.push(adminItem);
+            }
+          });
+        }
+      }
+      if(module.id === "b53819ee-2088-4e8a-be68-2e4b1d82cebb"){ //Monitoreo
+        for(let component of module.components){
+          component.elements.forEach((element:any) => {
+            const adminItem = AdminElement.find((item) => item.id === element.id);
+            if(adminItem){
+              this.adminList.push(adminItem);
+            }
+          });
+        }
+      }
+    }
   }
 
   ngOnInit(): void {
